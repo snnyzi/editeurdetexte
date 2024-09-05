93906ad3d362d7fbd543b293a10a091196fee861
carceles <carcelesfranck@gmail.com>
2024-09-05 08:30:05 +0200

    premier essai sur le remplissage de la fonction majuscules
    création de la classe CommandeMajuscules
    
    ajout du cas ou le texte est majuscules dans la classe CommandeFactory

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -26,6 +26,10 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    public void majuscules(int debut, int fin) {
+       remplacer(debut,fin,texte.substring(debut,fin).toUpperCase());
+    }
+
     @Override
     public String toString() {
         return this.texte;
